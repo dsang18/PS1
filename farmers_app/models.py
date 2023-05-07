566ce24ecb3a03ace7145aadaad8b4fa5b837fe8
dsang18 <darshilsanghvi1818@gmail.com>
2023-05-07 21:55:43 +0530

    inital commit

--- farmers_app/models.py
+++ farmers_app/models.py
@@ -1,40 +1,40 @@
 from django.db import models
 from django.contrib.auth.hashers import make_password
 
-class Customer(models.Model):
-    id = models.AutoField(primary_key=True)
-    fullname = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    phone = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    address = models.TextField(max_length=500)
+# class Customer(models.Model):
+#     id = models.AutoField(primary_key=True)
+#     fullname = models.CharField(max_length=100)
+#     password = models.CharField(max_length=100)
+#     phone = models.CharField(max_length=100)
+#     email = models.CharField(max_length=100)
+#     address = models.TextField(max_length=500)
 
-    def save(self):
-        self.password = make_password(self.password)
-        super(Customer, self).save()
+#     def save(self):
+#         self.password = make_password(self.password)
+#         super(Customer, self).save()
 
-    def __str__(self):
-        return self.fullname
+#     def __str__(self):
+#         return self.fullname
     
 
-class Farmer(models.Model):
-    id = models.AutoField(primary_key=True)
-    fullname = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    phone = models.CharField(max_length=100)
+# class Farmer(models.Model):
+#     id = models.AutoField(primary_key=True)
+#     fullname = models.CharField(max_length=100)
+#     password = models.CharField(max_length=100)
+#     phone = models.CharField(max_length=100)
 
 
-    def save(self):
-        self.password = make_password(self.password)
-        super(Farmer, self).save()
+#     def save(self):
+#         self.password = make_password(self.password)
+#         super(Farmer, self).save()
 
-    def __str__(self):
-        return self.fullname
+#     def __str__(self):
+#         return self.fullname
     
 
 
 class FoodGrains(models.Model):
-    farmer = models.ForeignKey(Farmer, db_column='farmer', on_delete=models.CASCADE)
+    farmer_mail = models.CharField(max_length=100)
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     quantity = models.IntegerField()
@@ -45,6 +45,6 @@ class FoodGrains(models.Model):
     
 
 class cart(models.Model):
-    customer = models.ForeignKey(Customer, db_column='customer', on_delete=models.CASCADE)
+    customer_mail = models.CharField(max_length=100)
     FoodGrains = models.ForeignKey(FoodGrains, db_column='in_cart_item', on_delete=models.CASCADE)
     quantity = models.IntegerField()
